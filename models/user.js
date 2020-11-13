4dcdffc68b238b9ba514adbb7ce46a71e8440213
Temidayo <akinsemoyin27@gmail.com>
2020-11-13 14:53:25 +0000

    version 2, improve user experience

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const passportLocalMongoose = require("passport-local-mongoose");
 
 const UserSchema = new mongoose.Schema({
 	username: {type: String, unique: true, required: true},
-	password: String,
+	password: {type: String,  required: true},
 	email: {type: String, unique: true, required: true},
 	resetPasswordToken: String,
 	resetPasswordExpires: Date,
